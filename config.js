b8e00cc6b5ddd4e77db75fb9df21665f1d47853e
Dondi Imperial <dondi@cpdone.com>
2016-12-07 13:46:26 +0800

    Proper segregation of configuration names

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
+
 var config = {};
 
 config.EXPRESS_PORT = parseInt(process.env.EXPRESS_PORT || '3000');
 config.JSON_HOST = process.env.JSON_HOST || 'jsonplaceholder.typicode.com';
 config.JSON_PORT = process.env.JSON_PORT || '';
 
-config.HOST = process.env.HOST || 'localhost';
+config.GOALS_HOST = process.env.GOALS_HOST || 'localhost';
+config.LEGACY_HOST = process.env.LEGACY_HOST || 'localhost';
 config.GOALS_PORT = process.env.GOALS_PORT || '9000';
-config.CPDONE_PORT = process.env.CPDONE_PORT || '12222';
+config.LEGACY_PORT = process.env.LEGACY_PORT || '12222';
 
 module.exports = config;
--- data/utilities/httpRequestOption.js
+++ data/utilities/httpRequestOption.js
@@ -19,16 +19,16 @@ import extend from 'extend';
 export default function(serviceType, method, route, headers) {
 	switch (serviceType) {
 		case CpdoneApiEnum.GOAL: 
-			return new httpRequestOptions(method, config.HOST, config.GOAL_PORT, route, headers);
+			return new httpRequestOptions(method, config.GOALS_HOST, config.GOALS_PORT, route, headers);
 			break;
 		case CpdoneApiEnum.LEGACY:
-			return new httpRequestOptions(method, config.HOST, config.CPDONE_PORT, route, headers);
+			return new httpRequestOptions(method, config.LEGACY_HOST, config.LEGACY_PORT, route, headers);
 			break;
 		case CpdoneApiEnum.VIEWER:
 			return new httpRequestOptions(method, config.JSON_HOST, config.JSON_PORT, route, headers);
 			break;
 		default:
-			return new httpRequestOptions(method, config.HOST, config.GOALS_PORT, route, headers);
+			return new httpRequestOptions(method, config.LEGACY_HOST, config.LEGACY_PORT, route, headers);
 	}
 }
 
