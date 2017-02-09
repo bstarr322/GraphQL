e88505244d3a549f62fd2e930d59296bd08c0742
Mark Daniel Alfajaro <mark.alfajaro@cpdone.com>
2017-02-09 13:12:38 +0800

    added new viewer type

---
+++ data/models/goalsApi/cpdTeamActivityType.js
@@ -0,0 +1,25 @@
+/**
+ * Model for getting cpd user goals from cpdone web service api.
+ * 
+ */
+
+import {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLNonNull,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLList
+} from 'graphql';
+
+
+import membershipType from '../legacyApi/membershipType.js';
+import cpdGoalUsersType from './cpdGoalUsersType.js';
+
+export default new GraphQLObjectType ({
+	name: 'CpdTeamActivity',
+	fields: function() { return {
+		cpdUsers: { type: new GraphQLList(cpdGoalUsersType)},
+		membership: { type: membershipType }
+	}},
+});
